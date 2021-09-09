dfe3c666b15f44c8143e2d8dd86f5ace679542ae
Bogdan Martseniuk <bogdan.martseniuk@vitechteam.com>
2021-09-09 15:49:40 +0300

    traverce tree algorithm

---
+++ src/main/kotlin/com/supper/generator/ParserUtil.kt
@@ -0,0 +1,4 @@
+package com.supper.generator
+
+class ParserUtil {
+}
---
+++ src/test/kotlin/com/supper/generator/ParserUtilKtTest.kt
@@ -0,0 +1,12 @@
+package com.supper.generator
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ParserUtilKtTest {
+
+    @Test
+    fun traverseJson() {
+    }
+}
